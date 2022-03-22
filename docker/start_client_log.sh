cafa58b41b63add144c6213bfa343cf5f0c849a2
Paul Geppert <paul.geppert@student.hpi.de>
2022-03-22 17:59:32 +0100

    Added my own config and start scripts, added helper script

--- docker/start_client_log.sh
+++ docker/start_client_log.sh
@@ -6,4 +6,4 @@ DATADIR="/sidelink/logs"
 OUTFILE="${DATADIR}/${MEASUREFILE}"
 PCAPFILE="${DATADIR}/${PCAPFILE}"
 
-/sidelink/build/srssl/src/srssl /sidelink/srssl/ue.conf.example --expert.phy.sidelink_id 2 --expert.phy.sidelink_master 0 --log.filename $OUTFILE --log.phy_level "warning" --log.phy_lib_level "warning" --pcap.enable 1 --pcap.filename $PCAPFILE --rf.device_args "type=b200,serial=320F331" --gw.tun_dev_name "tun_srssl_c"
+/sidelink/build/srssl/src/srssl /sidelink/srssl/ue.conf.gpa --expert.phy.sidelink_id 2 --expert.phy.sidelink_master 0 --log.filename $OUTFILE --log.phy_level "warning" --log.phy_lib_level "warning" --pcap.enable 1 --pcap.filename $PCAPFILE --rf.device_args "type=b200,serial=320F331" --gw.tun_dev_name "tun_srssl_c"
---
+++ gpa_start_server.sh
@@ -0,0 +1,1 @@
+UHD_IMAGES_DIR=/usr/share/uhd/images build/srssl/src/srssl srssl/ue.conf.gpa --expert.phy.sidelink_id 1 --expert.phy.sidelink_master 1 --rf.device_args "type=b200,serial=320F319" --gw.tun_dev_name "tun_srssl_m"
